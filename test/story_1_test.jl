d883b31aa18b5bb994aeaf7b6644c8af83132102
Evgeny Metelkin <metelkin@insysbio.com>
2021-07-13 14:08:27 +0300

    minor test fix

--- test/story_1_test.jl
+++ test/story_1_test.jl
@@ -1,7 +1,7 @@
 platform = load_platform("$HetaSimulatorDir/cases/story_1", rm_out = false);
 model = platform.models[:nameless];
 
-cond = Condition(model; tspan = (0., 200.), saveat = [0.0, 150., 250.])
+cond = HetaSimulator.Condition(model; tspan = (0., 200.), saveat = [0.0, 150., 250.])
 s = sim(cond)
 mcs = mc(cond, [:k2=>Normal(1e-3,1e-4), :k3=>Normal(1e-4,1e-5)], 1000)
 
