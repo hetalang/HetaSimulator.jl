ca37d90d6b5b8473564a9c87aecb8f21bacee6bb
Ivan <iv.borissov@gmail.com>
2022-02-09 18:54:01 +0300

    heta_update removed from tests

--- test/heta_test.jl
+++
@@ -1,2 +0,0 @@
-# Heta connector tests
-heta_update()
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,6 @@ function test_show(t)
 end
 
 @testset "HetaSimulator" begin
-  @testset "Heta build" begin include("heta_test.jl") end
   @testset "Single-compartment model without events" begin include("single_comp_test.jl") end
 end
 
