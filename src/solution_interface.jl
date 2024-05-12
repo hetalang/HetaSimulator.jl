4eb9ae6f24d0837f4106d06ce7655531f6c09a50
Ivan <iv.borissov@gmail.com>
2024-05-12 14:19:24 +0300

    DataFrame docs added

--- src/solution_interface.jl
+++ src/solution_interface.jl
@@ -41,6 +41,20 @@ function DataFrame(s::Simulation; vars=observables(s), parameters_output::Vector
   return df
 end
 
+"""
+    DataFrame(s::Simulation; 
+      vars=observables(s), parameters_output::Vector{Symbol}=Symbol[], iter::Union{Int,Nothing}=nothing)
+
+Converts simulation results of type `SimResult`, `MCResult`, etc  to `DataFrame`.
+
+Example: `DataFrame(s)`
+
+Arguments:
+
+- `s` : simulation results of type [`SimResult`](@ref), [`MCResult`](@ref) or `Vector` containing them
+- `parameters_output` : parameters provided as kwarg `parameters` to `sim` or `mc` functions, which should be included in `DataFrame`. Default is `empty`
+- `iter` : `Int` iteration id, which should be included in `DataFrame`. Default is `nothing`
+"""
 function DataFrame(sr::SimResult; kwargs...)
   df = DataFrame(sr.sim; kwargs...)
 
