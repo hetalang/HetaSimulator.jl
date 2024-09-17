9cf8bb57aa097d6f9d274ed863bf10a8d581f5ca
ivborissov <iv.borissov@gmail.com>
2024-09-17 11:14:01 +0300

    artifacts build updated

--- artifacts_scripts/build_artifacts.jl
+++ artifacts_scripts/build_artifacts.jl
@@ -5,10 +5,17 @@ const HETA_COMPILER_RELEASE = "v0.8.7"
 
 const artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
-for os in ("linux", "windows", "macos")
+platforms = [
+  Artifacts.Platform("x86_64", "linux"),
+  Artifacts.Platform("x86_64", "windows"),
+  Artifacts.Platform("x86_64", "macos"),
+  Artifacts.Platform("aarch64", "macos")
+]
 
+for platform in platforms
+
+  os  = platform.tags["os"]
   url = "https://github.com/hetalang/heta-compiler/releases/download/$HETA_COMPILER_RELEASE/heta-compiler-$os.tar.gz"
-  platform = Artifacts.Platform("x86_64", os)
 
   add_artifact!(
       artifacts_toml,
@@ -19,13 +26,3 @@ for os in ("linux", "windows", "macos")
       lazy = false,
   )
 end
-
-# add aarch64 for macos
-add_artifact!(
-  artifacts_toml,
-  "heta_app",
-  "https://github.com/hetalang/heta-compiler/releases/download/$HETA_COMPILER_RELEASE/heta-compiler-macos.tar.gz";
-  platform = Artifacts.Platform("aarch64", "macos"),
-  force = true,
-  lazy = false,
-)
