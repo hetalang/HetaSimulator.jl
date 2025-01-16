31d19500b184a213a8fe0a81823c4a2f8b3a945f
Ivan <iv.borissov@gmail.com>
2025-01-16 14:27:52 +0200

    fix obj value 2

--- test/single_comp_test.jl
+++ test/single_comp_test.jl
@@ -92,6 +92,6 @@ fres = fit([:one=>fscn1, :two=>fscn2], [:k1=>0.01, :sigma1=>1.0], progress=:sile
 @test length(measurements(fscn1)) == 24
 @test length(measurements(fscn2)) == 24
 @test status(fres) == :FTOL_REACHED
-@test obj(fres) ≈ 146.05
+@test isapprox(obj(fres), 146.05; atol=1e-2)
 @test typeof(optim(fres)) == Vector{Pair{Symbol, Float64}}
 @test length(optim(fres)) == 2
