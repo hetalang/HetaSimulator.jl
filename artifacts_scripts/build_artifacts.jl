c0b26a4a9270a7b07700eccff82ae6787a778c56
Ivan <iv.borissov@gmail.com>
2025-06-30 16:28:56 +0300

    Artifacts upd to 0.9.6

--- artifacts_scripts/build_artifacts.jl
+++ artifacts_scripts/build_artifacts.jl
@@ -1,7 +1,7 @@
 using ArtifactUtils
 using Pkg.Artifacts
 
-const HETA_COMPILER_RELEASE = "v0.9.6-alfa.2"
+const HETA_COMPILER_RELEASE = "v0.9.6"
 
 const artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
