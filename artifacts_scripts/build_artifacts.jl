34ee21014c5b8406517b22aa8d656f9aa73cb919
Ivan <iv.borissov@gmail.com>
2025-07-10 19:16:58 +0300

    artifacts

--- artifacts_scripts/build_artifacts.jl
+++ artifacts_scripts/build_artifacts.jl
@@ -1,7 +1,7 @@
 using ArtifactUtils
 using Pkg.Artifacts
 
-const HETA_COMPILER_RELEASE = "v0.9.6"
+const HETA_COMPILER_RELEASE = "arm5"
 
 const artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
@@ -15,7 +15,19 @@ platforms = [
 for platform in platforms
 
   os  = platform.tags["os"]
-  url = "https://github.com/hetalang/heta-compiler/releases/download/$HETA_COMPILER_RELEASE/heta-compiler-$os.tar.gz"
+  if os == "windows"
+    os = "win"
+  end
+  arch = platform.tags["arch"]
+  if arch == "x86_64"
+    arch = "x64"
+  elseif arch == "aarch64"  
+    arch = "arm64"
+  else
+    error("Unsupported architecture: $arch")
+  end
+    
+  url = "https://github.com/hetalang/heta-compiler/releases/download/$HETA_COMPILER_RELEASE/heta-compiler-$os-$arch.tar.gz"
 
   add_artifact!(
       artifacts_toml,
