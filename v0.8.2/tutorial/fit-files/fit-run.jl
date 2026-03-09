7c40e383c5a106910f845bc44f9ddec769dd44cc
Documenter.jl <documenter@juliadocs.github.io>
2026-03-09 14:36:01 +0000

    build based on c93073a

---
+++ v0.8.2/tutorial/fit-files/fit-run.jl
@@ -0,0 +1,83 @@
+# the code was tested on v0.4.2
+# reinstall HetaSimulator
+# ] add https://github.com/hetalang/HetaSimulator.jl
+
+using HetaSimulator, Plots
+
+p = load_platform(".")
+
+# load scenarios
+scn_df = read_scenarios("./scenarios.csv")
+add_scenarios!(p, scn_df)
+
+### Load measurements
+
+# see table measurements.csv describing data with unknown variance
+# see more detales in https://hetalang.github.io/HetaSimulator.jl/stable/table-formats/measurement/
+measurements_df = read_measurements("./measurements.csv")
+add_measurements!(p, measurements_df)
+
+# display platform content
+p
+
+### simulate all
+res = sim(p)
+
+# plot all default
+plot(res)
+# plotd = plot(res)
+# savefig(plotd, "fig.png")
+
+# plot C1, C2 in log scale
+plot(res, vars=[:C1,:C2], yscale=:log10, ylim=(1e-3, 1e3))
+# plotd = plot(res, vars=[:C1,:C2], yscale=:log10, ylim=(1e-3, 1e3))
+# savefig(plotd, "fig.png")
+
+### Fitting
+
+# fitted parameters
+to_fit = [
+    :kabs => 8.0,
+    :Q => 4.0,
+    :kel => 2.2,
+    :sigma1 => 0.1,
+    :sigma2 => 0.1,
+    :sigma3 => 0.1,
+]
+res_optim = fit(p, to_fit) # default fitting
+
+# optimal parameters
+optim(res_optim)
+
+# check fitting quality 
+res = sim(p, parameters = optim(res_optim))
+plot(res, yscale=:log10, ylims=(1e-3,1e2))
+# plotd = plot(res_optim, vars=[:C1,:C2], yscale=:log10, ylims=(1e-3,1e2))
+# savefig(plotd, "fig.png")
+
+### Fitting with parameters table
+
+# read parameters from table
+params_df = read_parameters("./parameters.csv")
+res_optim = fit(p, params_df)
+
+### additional optimization-specific options
+
+# see also https://nlopt.readthedocs.io/en/latest/NLopt_Algorithms/
+res_optim = fit(
+    p, 
+    params_df, 
+    fit_alg = NLopt.LN_SBPLX(), 
+    ftol_abs = 1e-5, 
+    ftol_rel = 0, 
+    maxiters = 10^6
+)
+optim(res_optim)
+
+#= see API docs
+fit_alg : fitting algorithm. Default is :LN_NELDERMEAD
+ftol_abs : absolute tolerance on objective value. Default is 0.0
+ftol_rel : relative tolerance on objective value. Default is 1e-4
+maxiters : maximum number of objective evaluations. Default is 1e4
+maxtime : maximum optimization time (in seconds). Default is 0
+=#
